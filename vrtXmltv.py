de8337ac2e37512922c013e9e720b23fb11988f2
depeje <github@depeje.net>
2019-03-23 22:18:58 +0100

    Auto detect how many days of guide data are available

--- vrtXmltv.py
+++ vrtXmltv.py
@@ -27,7 +27,10 @@ class Vrtnu:
             return
 
         self.jsonString = response.json()
-        self.inited = True
+        if len(self.jsonString) > 0:
+            self.inited = True
+        else:
+            self.inited = False
 
     def getListForChannel(self, channel):
         return self.jsonString.get(self._vrt_channel_map.get(channel))
@@ -72,52 +75,52 @@ if __name__ == '__main__':
     now = datetime.now()
     xmlDoc = Xml()
 
-    vrts = Vrtnu(now, 0), Vrtnu(now, 1), Vrtnu(now, 2), Vrtnu(now, 3), Vrtnu(now, 4), Vrtnu(now, 5), Vrtnu(now, 6)
+    advance = 1
+    vrt = Vrtnu(now, advance)
+    while vrt.inited == True:
+        for channel in "xmltv.canvas", "xmltv.een", "xmltv.ketnet":
+            timeTable = vrt.getListForChannel(channel)
+            lastStartInDay = 0
+            lastStop = 0
+            startDayAdder = 0
+            for program in timeTable:
+                stopDayAdder = 0
+                startInDay = int(program.get("start").replace(":",""))*100
+                stopInDay = int(program.get("end").replace(":",""))*100
 
-    for channel in "xmltv.canvas", "xmltv.een", "xmltv.ketnet":
-        for advance in 1, 2, 3, 4, 5, 6:
-            if vrts[advance].inited == True:
-                timeTable = vrts[advance].getListForChannel(channel)
-                lastStartInDay = 0
-                lastStop = 0
-                startDayAdder = 0
-                for program in timeTable:
-                    stopDayAdder = 0
-                    startInDay = int(program.get("start").replace(":",""))*100
-                    stopInDay = int(program.get("end").replace(":",""))*100
+                if startInDay < lastStartInDay:
+                    startDayAdder = 1
 
-                    if startInDay < lastStartInDay:
-                        startDayAdder = 1
+                if stopInDay < startInDay:
+                    stopDayAdder = 1
 
-                    if stopInDay < startInDay:
-                        stopDayAdder = 1
+                startTime = now + timedelta(days=(advance + startDayAdder))
+                startDaySeconds = startTime.year * 10000000000 \
+                                  + startTime.month * 100000000 \
+                                  + startTime.day * 1000000
+                start = startDaySeconds + startInDay
 
-                    startTime = now + timedelta(days=(advance + startDayAdder))
-                    startDaySeconds = startTime.year * 10000000000 \
-                                      + startTime.month * 100000000 \
-                                      + startTime.day * 1000000
-                    start = startDaySeconds + startInDay
+                stopTime = startTime + timedelta(days=stopDayAdder)
+                stopDaySeconds = stopTime.year * 10000000000 \
+                                  + stopTime.month * 100000000 \
+                                  + stopTime.day * 1000000
+                stop = stopDaySeconds + stopInDay
 
-                    stopTime = startTime + timedelta(days=stopDayAdder)
-                    stopDaySeconds = stopTime.year * 10000000000 \
-                                      + stopTime.month * 100000000 \
-                                      + stopTime.day * 1000000
-                    stop = stopDaySeconds + stopInDay
+                # VRT NU sometimes has overlapping programs, which mythtv can't handle.
+                if start < lastStop:
+                    start = lastStop
+                lastStop = stop
 
-                    # VRT NU sometimes has overlapping programs, which mythtv can't handle.
-                    if start < lastStop:
-                        start = lastStop
-                    lastStop = stop
+                lastStartInDay = startInDay
+                xmlDoc.addProgramme(start,
+                                    stop,
+                                    channel,
+                                    program.get("title"))
+        advance = advance + 1
+        vrt = Vrtnu(now, advance)
 
-                    lastStartInDay = startInDay
-                    xmlDoc.addProgramme(start,
-                                        stop,
-                                        channel,
-                                        program.get("title"))
-
-            else:
-                if advance == 1:
-                    exit(1)
+    if advance == 1:
+        exit(1)
 
     xmlDoc.output()
     sys.stdout.write("\n")
